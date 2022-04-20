d726fb09c7c295a1d9d87f334287195ad18cfefa
DannySilv <danielesilvestre@outlook.it>
2022-04-20 13:18:58 +0200

    Completed Jsnack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,1 +1,16 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
+
+const numberList = [];
+const userNumber = parseInt(prompt("Dammi un numero"));
+let counter = 1;
+let cubeNumber;
+
+
+while (counter <= userNumber) {
+    cubeNumber = counter * counter * counter;
+    console.log(cubeNumber);
+    counter++;
+    numberList.push(cubeNumber);
+}
+
+console.log(numberList);
